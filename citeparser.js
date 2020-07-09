b54352fa90e000e36e166186346b04134b52fde2
yap cheah shen <yapcheahshen@gmail.com>
2020-07-09 21:59:51 +0630

    parse PED to PTS cite convert to CAP qnum

---
+++ citeparser.js
@@ -0,0 +1,61 @@
+'use strict';
+const {parseRoman}=require("pengine");
+/* parse Sutta Central PED citation */
+
+const bookVolPage=(m,book,roman,page)=>{
+	const v=parseRoman(roman);
+	book=book.toLowerCase().replace("pts","ps")
+	const bk=book.indexOf("-")>0?book.replace("-",v):book+v;
+
+	return bk+","+page;
+}
+const bookPage=(m,book,page)=>{
+	const bk=book.toLowerCase()
+		.replace("dhs","ds")
+		.replace("netti","ne")
+		.replace("tha-ap","thap");
+
+	return bk.replace("-","0")+","+page;
+}
+
+const bookGroupGatha=(m,book,group,gatha)=>{
+	return book.toLowerCase()+"_"+(gatha)+"g"+parseRoman(group);
+}
+
+const bookVaggaVatthuGatha=(m,book,vagga,vatthu,gatha)=>{
+	return book.toLowerCase()+parseRoman(vagga)+"."+vatthu+"."+gatha;
+}
+const patterns=[
+	[/^(V)in\.([iv]{1,3})\.(\d+)/	, bookVolPage],
+	[/^([DMSA])N\.([i]{1,3})\.(\d+)/, bookVolPage],
+	[/^(J)a\.([iv]{1,3})\.(\d+)/	, bookVolPage],
+	[/^(Dhp-a|Pts|Dhp-a)\.([iv]{1,3})\.(\d+)/	,bookVolPage],
+
+	[/^(Dhp-a|Pts|Dhp-a)\.([iv]{1,3})\.(\d+)/	,bookVolPage],
+
+	[/^(Bv)\.([ivx]{1,5})\.(\d+)/	,bookGroupGatha],
+
+	[/^(Vb|Pp|Ud|Mil|Kv|Iti|Mnd|Ne|Netti)\.(\d+)/,bookPage],
+	[/^(Vv-a|Vb-a|Pv-a|Pp-a|Kv-a|Dhs-a|Snp-a|Tha-ap)\.(\d+)/,bookPage],
+
+	//[/^(Pv-a)\.(\d+)/,bookPage],
+	//[/^(Vb)\.(\d+)/,bookPage],
+	
+	[/^(Pv|Cp)\.([iv]{1,3})\.(\d+)#(\d+)/,bookVaggaVatthuGatha],
+	[/^Snp\.(\d+)/,"snp_$1g1"],
+	[/^Dhp\.(\d+)/,"dhp_$1"],
+	[/^Vv\.(\d+)#(\d+)/,"vv$1.$2"],
+	[/^Dhs\.(\d+)/,"ds_$1g3"],
+	[/^Th([ai])g\.(\d+)/,"th$1g_$2"],
+]
+const parsePEDCite=cite=>{
+	for (var i=0;i<patterns.length;i++) {
+		const pat=patterns[i][0];
+		const func=patterns[i][1];
+		const o=cite.replace(pat,func);
+		if (o!==cite) return o;
+	}
+	return cite;
+}
+
+module.exports={parsePEDCite};
---
+++ testciteparser.js
@@ -0,0 +1,66 @@
+'use strict'
+const {parsePEDCite}=require("./citeparser");
+let failed=0,passed=0;
+const assert=(a,b,testname)=>{
+	if (a!==b) {
+		testname&&console.log('test:',testname);
+		console.log('expecting',a);
+		console.log('got',b);
+		failed++;		
+	} else passed++
+}
+
+//unsupported
+//Sdhp Mhvs Dhtp Mvu Avs Dpvs Dhtm Tikp Abhp Pgdp Jtm Mhbv Lal Nd Kacc Duka-pa Asl
+
+const testdata=[
+	['Vin.i.188'	,'v1,188'	],
+	['Ja.vi.537'	,'j6,537'	],
+	['DN.i.51'		,'d1,51'	],
+	['MN.ii.133'	,'m2,133'	],
+	['SN.ii.90'		,'s2,90'	],
+	['AN.ii.209'	,'a2,209'	],
+	['Dhp-a.iii.46'	,'dhp3a,46'	],
+	['Pts.ii.241'	,'ps2,241'	],
+
+	['Vb.208'		,'vb,208'	],
+	['Vv-a.264'		,'vv0a,264'	],
+	['Vb-a.175'		,'vb0a,175'	],
+	['Pp.18'		,'pp,18'	],
+	['Pp-a.225'		,'pp0a,225'	],
+
+	['Dhp.294'		,'dhp_294'	],
+	['Dhp-a.iii.175','dhp3a,175'],
+	['Pv-a.151'		,'pv0a,151'	],
+	['Vv.37#1'		,'vv37.1'	], //vatthu, gatha
+	['Pv.iv.16#71'	,'pv4.16.71'],
+	['Cp.i.9#56'	,'cp1.9.56'	],
+	['Ud.62'		,'ud,62'	], 
+	['Bv.xxiii.23'	,'bv_23g23'	],
+	['Thag.1268'	,'thag_1268'],
+	['Thig.241'		,'thig_241'	],
+	['Mil.207'		,'mil,207'	],
+	['Kv.208'		,'kv,208'	],
+	['Iti.114'		,'iti,114'	],
+	['Dhs.146'		,'ds_146g3'	],
+	['Dhs-a.320'	,'ds0a,320'	],
+	['Mnd.114'		,'mnd,114'	],
+	['Tha-ap.103'	,'thap,103'	],
+	['Ne.54'		,'ne,54'	],
+	['Netti.54'		,'ne,54'	],
+	['Snp-a.160'	,'snp0a,160']
+	//Cnd.601
+	//Mil.ii.278
+/*  MN-a DN DN-a 
+Snp-a
+Thag-a  不符合 
+Vism  缺pts 頁碼 
+Cnd 數字太大
+Kp Kp-a  //kp 為了和 kp0a 對應，最前面多了 1.1-8  2.1-14 , 與PTS差2
+*/
+
+]
+testdata.forEach(test=>{
+	assert(test[1],parsePEDCite(test[0]))
+})
+console.log("passed",passed,"failed",failed)
